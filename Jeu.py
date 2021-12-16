1c42c0b3ecdb59633dbe255f8c068d08dfa569bb
n-ottavi-n <nottavi14@gmail.com>
2021-12-16 14:08:38 +0100

    threads+time

--- Jeu.py
+++ Jeu.py
@@ -7,17 +7,28 @@ Created on Thu Dec 16 09:48:12 2021
 
 import csv
 import matplotlib.pyplot as plt
-import numpy as np
+import PointConsommation
+import PointProduction
+import time
+import threading
 
-pasTemps=30 #en minutes, constante
-facteurTemps=1 #pour accelerer/ralentir
+step=0
+pasTemps=5 #en minutes, constante
+facteurTemps=10 #pour accelerer/ralentir
 courbeConso=[] #veritable, definie par l'admin
 courbeRenouvelable=[] #veritable, definie par l'admin
 courbeMain=[] #affiche la conso attendu et la production renouvelable
 consoGlobale=0 #mise a jour tous les pas
 prodGloabale=0 #mise a jour tous les pas
+
 a=49 #debut de la courbe
 b=98 #fin de la courbe
+
+pointsConso=[]
+pointsProd=[]
+
+etat=[]
+
 
 def createPointConso():
     return None
@@ -43,17 +54,32 @@ def displayCourbe():#pour le joueur seulement, affiche les courbes historiques
     for i in range(48):#48=une journee
         y_points.append(float(courbeMain[i][1]))  
         y2_points.append(float(courbeMain[i][2]))
-    print(y_points)
     plt.plot(y_points)
     plt.plot(y2_points)
     plt.xlabel('temps')
     plt.ylabel('MW')
     plt.show()
-
+    
+def updateEtat():
+    for i in range(b-a-1):
+        global step,consoGlobale
+        time.sleep((pasTemps/facteurTemps))
+        step+=1
+        consoGlobale=courbeMain[step][1]
+        print(consoGlobale)
+    
+def count():
+    for i in range(5):
+        print(i)
         
 
+loadCourbe(a,b)
+
+#met a jour la consommation tous les intervalles
+thread=threading.Thread(target=updateEtat)
+thread.start()
     
-    
+
 
 #initialisation depuis admin IHM
 
@@ -62,11 +88,18 @@ def displayCourbe():#pour le joueur seulement, affiche les courbes historiques
 nbPointsConso=0
 nbPointsProd=0
 
+nom,lieu=None,None
+puissance=0
+
 for i in range(nbPointsConso):
-    createPointConso()
+    newPc=PointConsommation(nom,lieu,puissance)
+    pointsConso.append(newPc)
+    etat.append(newPc)
     
 for i in range(nbPointsProd):
-    createPointProd()
+    newPp=PointProduction(nom,lieu,puissance)
+    pointsProd.append(newPp)
+    
     
 
     
@@ -76,6 +109,6 @@ for i in range(nbPointsProd):
 
 #Jeu avec user IHM
 
-loadCourbe(a,b)
 
-displayCourbe()
+
+#displayCourbe()
